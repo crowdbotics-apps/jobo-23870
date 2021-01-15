9d896e97bbe9700c6f82990b6a970df6ed0a5a34
Crowdbotics <team@crowdbotics.com>
2021-01-15 23:44:27 +0000

    Update components

--- src/features/BlankScreen0193610/screens/index.js
+++ src/features/BlankScreen0193610/screens/index.js
@@ -21,12 +21,34 @@ export class Blank extends React.Component {
   constructor(props) {
     super(props)
 
-    this.state = {}
+    this.state = { Switch_15: true }
   }
   static navigationOptions = ({ navigation }) => {
     return { headerLeft: <SlideMenuIcon navigationProps={navigation} /> }
   }
-  render = () => <View style={styles.View_1} />
+  render = () => (
+    <View style={styles.View_1}>
+      <Button title="Press me!" onPress={() => alert("Pressed!")} />
+      <Button title="Press me!" onPress={() => alert("Pressed!")} />
+      <Button title="Press me!" onPress={() => alert("Pressed!")} />
+      <Button title="Press me!" onPress={() => alert("Pressed!")} />
+      <Button title="Press me!" onPress={() => alert("Pressed!")} />
+      <Button title="Press me!" onPress={() => alert("Pressed!")} />
+      <Button title="Press me!" onPress={() => alert("Pressed!")} />
+      <Button title="Press me!" onPress={() => alert("Pressed!")} />
+      <Button title="Press me!" onPress={() => alert("Pressed!")} />
+      <Button title="Press me!" onPress={() => alert("Pressed!")} />
+      <Button title="Press me!" onPress={() => alert("Pressed!")} />
+      <Button title="Press me!" onPress={() => alert("Pressed!")} />
+      <Button title="Press me!" onPress={() => alert("Pressed!")} />
+      <Switch
+        trackColor={{ true: "#409EFF", false: "#C0CCDA" }}
+        style={styles.Switch_15}
+        value={this.state.Switch_15}
+        onValueChange={nextChecked => this.setState({ Switch_15: nextChecked })}
+      />
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
@@ -38,13 +60,29 @@ const styles = StyleSheet.create({
     borderTopWidth: -1,
     borderBottomWidth: -1,
     borderRadius: 6
-  }
+  },
+  Button_2: {},
+  Button_3: {},
+  Button_4: {},
+  Button_5: {},
+  Button_6: {},
+  Button_7: {},
+  Button_8: {},
+  Button_9: {},
+  Button_10: {},
+  Button_11: {},
+  Button_12: {},
+  Button_13: {},
+  Button_14: {},
+  Switch_15: { alignSelf: "flex-start" }
 })
 
 const mapStateToProps = state => {
   return {}
 }
+
 const mapDispatchToProps = () => {
   return {}
 }
+
 export default connect(mapStateToProps, mapDispatchToProps)(Blank)
