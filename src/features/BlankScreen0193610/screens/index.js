cbe6ac50dee1405c819d4719c577291751fef5af
Crowdbotics <team@crowdbotics.com>
2021-01-15 23:09:23 +0000

    Update components

---
+++ src/features/BlankScreen0193610/screens/index.js
@@ -0,0 +1,50 @@
+import React from "react"
+import {
+  View,
+  Image,
+  ImageBackground,
+  TouchableOpacity,
+  Text,
+  Button,
+  Switch,
+  TextInput,
+  StyleSheet,
+  ScrollView
+} from "react-native"
+import DateTimePicker from "react-native-datepicker"
+import Icon from "react-native-vector-icons/FontAwesome"
+import Slider from "@react-native-community/slider"
+import { CheckBox } from "react-native-elements"
+import { SlideMenuIcon } from "../../../navigator/slideMenuIcon"
+import { connect } from "react-redux"
+export class Blank extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {}
+  }
+  static navigationOptions = ({ navigation }) => {
+    return { headerLeft: <SlideMenuIcon navigationProps={navigation} /> }
+  }
+  render = () => <View style={styles.View_1} />
+}
+
+const styles = StyleSheet.create({
+  View_1: {
+    borderColor: "#7717ee",
+    borderWidth: -1,
+    borderLeftWidth: -1,
+    borderRightWidth: -1,
+    borderTopWidth: -1,
+    borderBottomWidth: -1,
+    borderRadius: 6
+  }
+})
+
+const mapStateToProps = state => {
+  return {}
+}
+const mapDispatchToProps = () => {
+  return {}
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Blank)
